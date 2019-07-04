9031765e299a5336802d5c13d18dfe5c0e0959af
panisterfatheroy <41014182+panisterfatheroy@users.noreply.github.com>
2019-07-04 12:28:10 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,22 @@
+<html>
+	<body>
+
+<!-- 
+		<?php 
+
+
+		if (isset($_POST['albumType'])){
+			echo 
+
+		}
+	
+		?> 
+ -->
+		
+		    <script type="text/javascript">
+				var str = "<?php echo $_POST['albumType'] ?>"; 
+				alert(str);
+				</script>
+		
+  </body>
+</html>
